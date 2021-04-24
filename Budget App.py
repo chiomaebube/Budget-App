93207fccad9ae2d671a96fc477e4d28609d92843
Chioma Oko <chiomaebubeoko@gmail.com>
2021-04-24 15:24:54 +0100

    update

--- Budget App.py
+++ Budget App.py
@@ -4,22 +4,26 @@ class Budget:
         self.category = category
         self.amt = amt
 
-    def deposit_funds(self):
+    def deposit_funds(self, amount, reason):
+        self.amount = amount
+        self.reason = reason
         return "This is a deposit method"
 
-    def calc_balance(self):
+    def calc_balance(self, amount, reason):
         return "This is a method to calculate the current balance of the category"
     
-    def withdraw_fund(self):
+    def withdraw_fund(self, amount, reason):
         return "This is a  method to withdraw funds for a category"
 
-    def transfer_funds(self):
+    def transfer_funds(self, amount, reason):
         return "This is a method to tranfer funds between categories"
 
 Clothing = Budget("Clothing", 1000)
 Food = Budget("Food", 1000)
 Entertainment = Budget("Entertainment", 1000)
 
+Food.deposit_funds(1000, "Salary")
+Food.withdraw_fund(200)
 
 print(Clothing.deposit_funds())
 print(Clothing.calc_balance())
